f235feac758e5d2166fb529df7e9ba030f1e1327
SvenH2000 <80902190+SvenH2000@users.noreply.github.com>
2022-02-16 10:47:12 +0100

    Add files via upload

--- McNemar/McNemar.py
+++ McNemar/McNemar.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 15 14:10:28 2022
+
 @author: svenh
 """
 
 from statsmodels.stats.contingency_tables import mcnemar
 import numpy as np
-pred_values_model1 = pred_values #Load in the predicted values (binary) for model 1
-pred_values_model2 = pred_values_2 #Load in the predicted values (binary) for model 2
-y_test = y_test #Load in the y_test model
+from numpy import genfromtxt
+pred_values_model1 = genfromtxt('C:/Users/svenh/OneDrive/Documents/Master/Seminar Case Studies/McNemar/pred_lr.csv', delimiter=',',skip_header=1)#Load in the predicted values (binary) for model 1
+pred_values_model2 = genfromtxt('C:/Users/svenh/OneDrive/Documents/Master/Seminar Case Studies/McNemar/pred_lr.csv', delimiter=',',skip_header=1) #Load in the predicted values (binary) for model 2
+y_test = genfromtxt('C:/Users/svenh/OneDrive/Documents/Master/Seminar Case Studies/McNemar/y_test.csv', delimiter=',',skip_header=1)  #Load in the y_test model
 model1_correctness = (pred_values_model1==y_test)
 model2_correctness = (pred_values_model2==y_test)
 
 contingency_table = np.zeros((2,2))
 for i in range(0,len(pred_values_model1)):
-	#if y_test[i]==1: #Dit begin uncomment als we willen testen op alleen de default cases
-	    if model1_correctness[i]==True and model2_correctness[i]==True:
-		contingency_table[0,0] += 1
-	    elif model1_correctness[i]==False and model2_correctness[i]==False:
-		contingency_table[1,1] += 1
-	    elif model1_correctness[i]==True and model2_correctness[i]==False:
-		contingency_table[0,1] += 1
-	    elif model1_correctness[i]==False and model2_correctness[i]==True:
-		contingency_table[1,0] += 1
+    if y_test[i]==1:
+        if model1_correctness[i]==True and model2_correctness[i]==True:
+            contingency_table[0,0] += 1
+        elif model1_correctness[i]==False and model2_correctness[i]==False:
+            contingency_table[1,1] += 1
+        elif model1_correctness[i]==True and model2_correctness[i]==False:
+            contingency_table[0,1] += 1
+        elif model1_correctness[i]==False and model2_correctness[i]==True:
+            contingency_table[1,0] += 1
         
 result = mcnemar(contingency_table, exact=False,correction=False)
 # summarize the finding
