1be08ead325b0901c0133254f190c860e98ce468
noordiepman <80516329+noordiepman@users.noreply.github.com>
2022-02-16 14:21:36 +0100

    Update XGBoost.py

--- Models/XGBoost.py
+++ Models/XGBoost.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import brier_score_loss
-from hmeasure import h_score
+#from hmeasure import h_score
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import PredefinedSplit
@@ -14,40 +14,41 @@ import xgboost as xgb
 
 start_time = datetime.now()
 
+
 # Not used here, but can be used for other models to get the same split
 def validation_splitter():
     # Load raw data
-    train = pd.read_csv('train_data.csv.gz', compression='gzip',  header=0, sep=',', quotechar='"')
-    
+    train = pd.read_csv('train_data.csv.gz', compression='gzip', header=0, sep=',', quotechar='"')
+
     # Factorize data
     train['country_code'] = pd.factorize(train['country_code'])[0]
     train['industry_code'] = pd.factorize(train['industry_code'])[0]
     train['size_class'] = pd.factorize(train['size_class'])[0]
     train['status_year'] = pd.factorize(train['status_year'])[0]
-    
+
     # Seperate labels and covariates
     X = train.drop(['y'], axis=1)
     y = train['y']
-    
+
     X_train, X_val, y_train, y_val = train_test_split(
-        X, y, test_size=15/85, random_state=42, stratify = y)
+        X, y, test_size=15 / 85, random_state=42, stratify=y)
 
     return X_train, y_train, X_val, y_val
+
 
 # =============================================================================
 # Performance metrics
 # =============================================================================
 
 def partial_gini(y, p):
-    
     # Select probabilites < 0.4
     output = pd.DataFrame()
-    output['y']=y
-    output['p']=p
+    output['y'] = y
+    output['p'] = p
     under4 = output[output["p"] < 0.4]
     y = under4['y']
     p = under4['p']
-    
+
     # Calculate Gini index
     # see https://www.kaggle.com/batzner/gini-coefficient-an-intuitive-explanation
     assert (len(y) == len(p))
@@ -56,62 +57,63 @@ def partial_gini(y, p):
     totalLosses = all[:, 0].sum()
     giniSum = all[:, 0].cumsum().sum() / totalLosses
     giniSum -= (len(y) + 1) / 2.
-    
+
     return giniSum / len(y)
 
+
 def get_performance(y, y_f, p):
-    
     # print performance metrics
     print('PG is ', partial_gini(y, p))
     print('BS is ', brier_score_loss(y, p))
     print('AUC is ', roc_auc_score(y, p))
-    print('H-measure is ', h_score(y.values, p))
+    #print('H-measure is ', h_score(y.values, p))
     print(confusion_matrix(y, y_f))
-    
+
+
 # =============================================================================
 # Data splitting
 # =============================================================================
 
 def get_data(small):
-    train = pd.read_csv('train_data.csv.gz', compression='gzip',  header=0, sep=',', quotechar='"')
-    test = pd.read_csv('test_data.csv.gz', compression='gzip',  header=0, sep=',', quotechar='"')
-    
+    train = pd.read_csv('train_data.csv.gz', compression='gzip', header=0, sep=',', quotechar='"')
+    test = pd.read_csv('test_data.csv.gz', compression='gzip', header=0, sep=',', quotechar='"')
+
     if small == True:
         n = 1000
         train = train.head(n=n)
         test = test.head(n=n)
-        
+
     # Factorize data
     train['country_code'] = pd.factorize(train['country_code'])[0]
     train['industry_code'] = pd.factorize(train['industry_code'])[0]
     train['size_class'] = pd.factorize(train['size_class'])[0]
     train['status_year'] = pd.factorize(train['status_year'])[0]
-    
+
     test['country_code'] = pd.factorize(test['country_code'])[0]
     test['industry_code'] = pd.factorize(test['industry_code'])[0]
     test['size_class'] = pd.factorize(test['size_class'])[0]
     test['status_year'] = pd.factorize(test['status_year'])[0]
-    
+
     # Seperate labels and covariates
     X = train.drop(['y'], axis=1)
     y = train['y']
 
     X_train, X_val, y_train, y_val = train_test_split(
-        X, y, test_size=15/85, random_state=42, stratify = y)
-    
-    X_train = pd.concat([X_train,X_val], ignore_index=True)
-    y_train = pd.concat([y_train,y_val], ignore_index=True)  
-    
-    train_size = int( len(train)*70/85 )
+        X, y, test_size=15 / 85, random_state=42, stratify=y)
+
+    X_train = pd.concat([X_train, X_val], ignore_index=True)
+    y_train = pd.concat([y_train, y_val], ignore_index=True)
+
+    train_size = int(len(train) * 70 / 85)
     test_size = len(train) - train_size
 
-    zeros = [0]*train_size
-    ones = [-1]*test_size
-    validation_index = zeros+ones
-    
+    zeros = [0] * train_size
+    ones = [-1] * test_size
+    validation_index = zeros + ones
+
     X_test = test.drop(['y'], axis=1)
     y_test = test['y']
-    
+
     c = ['country_code',
          'industry_code',
          'size_class',
@@ -163,11 +165,11 @@ def get_data(small):
          'interest_coverage_ratio_0',
          'interest_coverage_ratio_1',
          'interest_coverage_ratio_2']
-    
+
     return X_train[c], y_train, X_test[c], y_test, validation_index
 
-X_train, y_train, X_test, y_test, validation_index = get_data(False)
 
+X_train, y_train, X_test, y_test, validation_index = get_data(False)
 
 print('Data loading Duration: {}'.format(datetime.now() - start_time))
 
@@ -176,31 +178,60 @@ print('Data loading Duration: {}'.format(datetime.now() - start_time))
 # =============================================================================
 start_time = datetime.now()
 
-weight = y_train*len(y_test)/sum(y_test)/2
+weight = y_train * len(y_test) / sum(y_test) / 2
 weight = weight.values
 weight[np.where(weight == 0)[0]] = 1
 
-model = xgb.XGBClassifier(n_estimators=500 )
-model.fit(X_train, y_train, sample_weight=weight, verbose=True)
+#implementing random grid search, following https://towardsdatascience.com/hyperparameter-tuning-the-random-forest-in-python-using-scikit-learn-28d2aa77dd74
+n_estimators = [int(x) for x in np.arange(100, 600, 100)]
+max_features = ['sqrt']
+max_depth = [int(x) for x in np.arange(1, 111, 5)]
+max_depth.append(None)
+min_samples_leaf = [int(x) for x in np.arange(2, 14, 2)]
+learning_rate = [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.75]
+gamma = [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.75]
+class_weight = ['balanced', 'balanced_subsample']
+random_grid = {'n_estimators': n_estimators, #deze
+                'max_features': max_features,
+                'max_depth': max_depth, #deze
+                'min_samples_leaf': min_samples_leaf,
+                'class_weight': class_weight,
+                'learning_rate': learning_rate, #deze
+                'gamma': gamma #deze
+}
+print(random_grid)
+
+# # Run RFs
+model = xgb.XGBClassifier(random_state = 1)
+xgb_random = RandomizedSearchCV(estimator = model, param_distributions = random_grid,
+                                 n_iter = 10, cv = PredefinedSplit( validation_index ),
+                                 verbose=2, random_state=1, n_jobs = -1,
+                                 scoring = 'roc_auc')
+xgb_random.fit(X_train, y_train)
+best_random = xgb_random.best_estimator_
+print(best_random)
+
+#model = xgb.XGBClassifier(n_estimators=500, max_depth=, learning_rate=, gamma=)
+#model.fit(X_train, y_train, sample_weight=weight, verbose=True)
 
 # Get test performance
-pred_values = model.predict(X_test)
-prob_values = model.predict_proba(X_test)[:, 1]
+pred_values = best_random.predict(X_test)
+prob_values = best_random.predict_proba(X_test)[:, 1]
 get_performance(y_test, pred_values, prob_values)
 
 print('Modeling Duration: {}'.format(datetime.now() - start_time))
 
-importance = model.feature_importances_
+importance = best_random.feature_importances_
 ordered_importances = pd.DataFrame()
 ordered_importances['feature'] = X_train.columns
-ordered_importances['importance'] = importance 
+ordered_importances['importance'] = importance
 ordered_importances = ordered_importances.sort_values(ascending=False, by=['importance'])
 
-n =10
+n = 10
 some = ordered_importances.head(n)
 print(some)
 # save model
-model.save_model("xgboost.json")
+best_random.save_model("xgboost.json")
 
 # load model
 # model_xgb_2 = xgb.XGBClassifier()
