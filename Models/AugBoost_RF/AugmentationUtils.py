b7e07a7e2f6c1a70882fd29ee8343fe4d8fe9b14
noordiepman <80516329+noordiepman@users.noreply.github.com>
2022-03-07 13:06:01 +0100

    Update AugmentationUtils.py

--- Models/AugBoost_RF/AugmentationUtils.py
+++ Models/AugBoost_RF/AugmentationUtils.py
@@ -1,4 +1,6 @@
-
+#####
+# This code augments the features for the AugBoostRF. Therfore, it is imported by AugBoostRandomForest.py
+####
 from sklearn.utils import gen_batches, check_random_state
 
 from sklearn.decomposition import PCA
@@ -27,10 +29,6 @@ def get_transformed_params(X, y, n_features_per_subset, max_epochs, random_state
         random_state = np.random.randint(0,700)
     transforming_params = []
     for i, subset in enumerate(random_feature_subsets(X, n_features_per_subset, random_state=random_state)):
-        # if(augmentation_method=='nn')|(augmentation_method=='NN'):
-        #     model, nn_history = nn_augment_model(X[:, subset], y, max_epochs = max_epochs)
-        #     transforming_params.append((model, subset, nn_history))
-        # else:
             if(augmentation_method == 'pca')|(augmentation_method == 'PCA'):
                 pca = PCA()
                 model = pca.fit(X[:, subset])
@@ -38,15 +36,6 @@ def get_transformed_params(X, y, n_features_per_subset, max_epochs, random_state
             elif(augmentation_method=='rf')|(augmentation_method=='RF'):
                     rf = RandomForestRegressor(max_depth=5, random_state = 1,n_estimators=n_features_per_subset,
                     n_jobs=-1)
-                    # rf = RandomForestClassifier(bootstrap=True, ccp_alpha=0.0,
-                    #        class_weight='balanced_subsample', criterion='gini',
-                    #        max_depth=5, max_features='sqrt', max_leaf_nodes=None,
-                    #        max_samples=None, min_impurity_decrease=0.0,
-                    #        min_impurity_split=None, min_samples_split=2, 
-                    #        min_weight_fraction_leaf=0.0,
-                    #        n_estimators=10, n_jobs=-1, oob_score=False,
-                    #        random_state=1, verbose=0, warm_start=False)
-                    #classnames, indices = np.unique(y, return_inverse=True)
                     model = rf.fit(X[:, subset], y)
                     transforming_params.append((model, subset))
             else:
