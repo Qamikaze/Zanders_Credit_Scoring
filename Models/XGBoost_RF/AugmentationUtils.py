c6d5c7008d094ade23e87755d67887c450eeafc1
SvenH2000 <80902190+SvenH2000@users.noreply.github.com>
2022-03-07 15:18:59 +0100

    Update AugmentationUtils.py

--- Models/XGBoost_RF/AugmentationUtils.py
+++ Models/XGBoost_RF/AugmentationUtils.py
@@ -1,4 +1,6 @@
-
+#####
+# This code augments the features for the AugBoostRF. Therfore, it is imported by AugBoostRandomForest.py
+####
 from sklearn.utils import gen_batches, check_random_state
 
 from sklearn.decomposition import PCA
@@ -21,14 +23,12 @@ def augment(X, augmentation_matrix):
     return np.dot(X, augmentation_matrix)
 
 def get_transformed_params(X, y, n_features_per_subset, max_epochs, random_state=777, augmentation_method='nn'):
+    print(X.shape)
+    print(y.shape)
     if random_state == 777:
         random_state = np.random.randint(0,700)
     transforming_params = []
     for i, subset in enumerate(random_feature_subsets(X, n_features_per_subset, random_state=random_state)):
-        if(augmentation_method=='nn')|(augmentation_method=='NN'):
-            model, nn_history = nn_augment_model(X[:, subset], y, max_epochs = max_epochs)
-            transforming_params.append((model, subset, nn_history))
-        else:
             if(augmentation_method == 'pca')|(augmentation_method == 'PCA'):
                 pca = PCA()
                 model = pca.fit(X[:, subset])
@@ -36,15 +36,6 @@ def get_transformed_params(X, y, n_features_per_subset, max_epochs, random_state
             elif(augmentation_method=='rf')|(augmentation_method=='RF'):
                     rf = RandomForestRegressor(max_depth=5, random_state = 1,n_estimators=n_features_per_subset,
                     n_jobs=-1)
-                    # rf = RandomForestClassifier(bootstrap=True, ccp_alpha=0.0,
-                    #        class_weight='balanced_subsample', criterion='gini',
-                    #        max_depth=5, max_features='sqrt', max_leaf_nodes=None,
-                    #        max_samples=None, min_impurity_decrease=0.0,
-                    #        min_impurity_split=None, min_samples_split=2, 
-                    #        min_weight_fraction_leaf=0.0,
-                    #        n_estimators=10, n_jobs=-1, oob_score=False,
-                    #        random_state=1, verbose=0, warm_start=False)
-                    #classnames, indices = np.unique(y, return_inverse=True)
                     model = rf.fit(X[:, subset], y)
                     transforming_params.append((model, subset))
             else:
@@ -53,7 +44,7 @@ def get_transformed_params(X, y, n_features_per_subset, max_epochs, random_state
                     model = rp.fit(X[:, subset])
                     transforming_params.append((model, subset))
                 else:
-                    raise ValueError("`augmentation_method` must be `pca`, 'rp','rf or `nn`, but was %s" % augmentation_method)
+                    raise ValueError("`augmentation_method` must be `pca`, 'rp','rf' or `nn`, but was %s" % augmentation_method)
     return transforming_params
 
 
@@ -65,7 +56,8 @@ def get_transformed_matrix(X, transforming_params, augmentation_method):
             transformed_matrix[np.ix_([True] * len(X), subset)] = model.predict(X[:, subset])
         elif(augmentation_method == 'rf')|(augmentation_method == 'RF'):
                 model, subset = tup
-                transformed_matrix[np.ix_([True] * len(X), subset)] = FunctionTransformer(rf_apply, kw_args={"model": model}).transform(X[:, subset])
+                #transformed_matrix[np.ix_([True] * len(X), subset)] = FunctionTransformer(rf_apply, kw_args={"model": model}).transform(X[:, subset])
+                transformed_matrix[np.ix_([True] * len(X), subset)] = model.apply(X[:,subset])
         else:
             if(augmentation_method == 'pca')|(augmentation_method == 'PCA')|(augmentation_method == 'rp')|(augmentation_method == 'RP'):
                 model, subset = tup
